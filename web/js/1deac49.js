22ad947540c59c7b8ec9b08e3bb5be4019e5dd34
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2022-04-29 15:39:54 -0300

    Adicionado assets

---
+++ web/js/1deac49.js
@@ -0,0 +1,32 @@
+(function($){
+    var all = {
+        construct: function() {
+            this.events();
+        },
+        events: function() {
+            $('.inativar-participante').click(this.handleClickInativarParticipante);
+        },
+        handleClickInativarParticipante: function(){
+            var that = this;
+            bootbox.confirm('Deseja realmente remover esse participante?', function(result){
+                if (result) {
+                    $.post(
+                        Routing.generate('participante_inativar', {projetoPessoa: $(that).attr('data-participante')}),
+                        {},
+                        function(data){
+                            bootbox.alert(data.message, function(){
+                                if (data.status) {
+                                    location.reload(true);
+                                }
+                            });
+                        },
+                        'json'
+                    );
+                }
+            });
+        }
+    };
+    
+    all.construct();
+})(jQuery);
+
