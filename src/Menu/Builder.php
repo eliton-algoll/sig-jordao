32177809b351ae18d42290096c0e4b34cf213ece
Eliton Mendes <elitonmendes@bluems.com>
2025-02-17 23:38:44 -0300

    update builder

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -3,39 +3,51 @@
 namespace App\Menu;
 
 use Knp\Menu\FactoryInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Perfil;
+use App\Entity\Projeto;
 
-class Builder implements ContainerAwareInterface
+class Builder
 {
-    use ContainerAwareTrait;
+    private $factory;
+    private $tokenStorage;
+    private $authorizationChecker;
+    private $entityManager;
 
-    /**
-     * @param FactoryInterface $factory
-     * @param array $options
-     * @return \Knp\Menu\ItemInterface
-     */
-    public function mainMenu(FactoryInterface $factory, array $options)
+    public function __construct(
+        FactoryInterface $factory,
+        TokenStorageInterface $tokenStorage,
+        AuthorizationCheckerInterface $authorizationChecker,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->factory = $factory;
+        $this->tokenStorage = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->entityManager = $entityManager;
+    }
+
+    public function mainMenu(array $options): ItemInterface
     {
-        $securityChecker = $this->container->get('security.authorization_checker');        
-        
-        $menu = $factory->createItem('root');
+        $menu = $this->factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
-        
-        if ($securityChecker->isGranted(Perfil::ROLE_ADMINISTRADOR)) {
-            $this->menuAdministrador($menu);            
-        } elseif ($securityChecker->isGranted([Perfil::ROLE_COORDENADOR_PROJETO])) {
+
+        if ($this->authorizationChecker->isGranted(Perfil::ROLE_ADMINISTRADOR)) {
+            $this->menuAdministrador($menu);
+        } elseif ($this->authorizationChecker->isGranted(Perfil::ROLE_COORDENADOR_PROJETO)) {
             $this->menuCoordenadorProjeto($menu);
-        } elseif ($securityChecker->isGranted(Perfil::ROLE_COORDENADOR_GRUPO)) {
+        } elseif ($this->authorizationChecker->isGranted(Perfil::ROLE_COORDENADOR_GRUPO)) {
             $this->menuCoordenadorGrupo($menu);
-        } elseif ($securityChecker->isGranted(Perfil::ROLE_PRECEPTOR)) {
+        } elseif ($this->authorizationChecker->isGranted(Perfil::ROLE_PRECEPTOR)) {
             $this->menuPreceptor($menu);
-        } elseif ($securityChecker->isGranted(Perfil::ROLE_TUTOR)) {
+        } elseif ($this->authorizationChecker->isGranted(Perfil::ROLE_TUTOR)) {
             $this->menuTutor($menu);
-        } elseif ($securityChecker->isGranted(Perfil::ROLE_ESTUDANTE)) {
+        } elseif ($this->authorizationChecker->isGranted(Perfil::ROLE_ESTUDANTE)) {
             $this->menuEstudante($menu);
-        } elseif ($securityChecker->isGranted('ROLE_PREVIOUS_ADMIN')) {
+        } elseif ($this->authorizationChecker->isGranted('ROLE_PREVIOUS_ADMIN')) {
             if (in_array(Perfil::ROLE_COORDENADOR_PROJETO, $this->getUser()->getRoles())) {
                 $this->menuCoordenadorProjeto($menu);
             }
@@ -43,189 +55,109 @@ class Builder implements ContainerAwareInterface
         return $menu;
     }
 
-    /**
-     * @param FactoryInterface $factory
-     * @param array $options
-     * @return mixed
-     */
-    public function menuExterno(FactoryInterface $factory, array $options)
+    public function menuExterno(array $options): ItemInterface
     {
-        $menu = $factory->createItem('root');
+        $menu = $this->factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
 
-        $menu->addChild('Informe de Rendimento', array('route' => 'informe-rendimento'));
-        $menu->addChild('Fale Conosco', array('route' => 'fale_conosco'));
+        $menu->addChild('Informe de Rendimento', ['route' => 'informe-rendimento']);
+        $menu->addChild('Fale Conosco', ['route' => 'fale_conosco']);
 
         return $menu;
     }
-    
-    public function menu(FactoryInterface $factory, array $options)
+
+    public function menu(array $options): ItemInterface
     {
-        $securityChecker = $this->container->get('security.authorization_checker');
-        
-        $menu = $factory->createItem('root');
+        $menu = $this->factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
-        
-        $menu->addChild('Alterar Perfil', array('route' => 'default_selecionar_perfil'));
-        if ($securityChecker->isGranted('ROLE_PREVIOUS_ADMIN')) {
-            $menu->addChild('Sair da Representação', array('uri' => '/?_switch_user=_exit'));
+
+        $menu->addChild('Alterar Perfil', ['route' => 'default_selecionar_perfil']);
+
+        if ($this->authorizationChecker->isGranted('ROLE_PREVIOUS_ADMIN')) {
+            $menu->addChild('Sair da Representação', ['uri' => '/?_switch_user=_exit']);
         } else {
-            $menu->addChild('Sair', array('route' => 'logout'));
+            $menu->addChild('Sair', ['route' => 'logout']);
         }
-        
+
         return $menu;
     }
 
-    /**
-     * @param \Knp\Menu\ItemInterface $menu
-     */
-    private function menuAdministrador(\Knp\Menu\ItemInterface $menu)
+    private function menuAdministrador(ItemInterface $menu)
     {
-        $menu->addChild('Programas', array('route' => 'programa'));
-        $menu->addChild('Projetos', array('route' => 'projeto'));
-        $menu->addChild('Participantes', array('route' => 'participante'));
-        $menu->addChild('Folha de pagamento', array('route' => 'folha_pagamento'));        
-        
-        $menu->addChild('Relatorios')->setAttribute('dropdown', true);
-        $menu['Relatorios']->addChild('Bolsas', array('route' => 'relatorio_pagamento'));
-        $menu['Relatorios']->addChild('Bolsas não autorizadas', array('route' => 'relatorio_pagamento_nao_autorizado'));
-        $menu['Relatorios']->addChild('Folha de Pagamento', array('route' => 'relatorio_folha_pagamento'));
-        $menu['Relatorios']->addChild('Gerencial de Pagamento', array('route' => 'relatorio_gerencial_pagamento'));
-        $menu['Relatorios']->addChild('Gerencial de Participantes', array('route' => 'relatorio_gerencial_participante'));
-        $menu['Relatorios']->addChild('Participantes', array('route' => 'relatorio_participante'));
-        $menu['Relatorios']->addChild('Projetos', array('route' => 'relatorio_projeto'));
-        
-        $menu->addChild('Avaliação de Atividades')->setAttribute('dropdown', true);
-        $menu['Avaliação de Atividades']->addChild('Gerenciar Formulário de Atividades', array('route' => 'gerenciar_formulario_atividade'));
-        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', array('route' => 'tramita_formulario_atividade'));
-        
+        $menu->addChild('Programas', ['route' => 'programa']);
+        $menu->addChild('Projetos', ['route' => 'projeto']);
+        $menu->addChild('Participantes', ['route' => 'participante']);
+        $menu->addChild('Folha de pagamento', ['route' => 'folha_pagamento']);
+
+        $menu->addChild('Relatórios')->setAttribute('dropdown', true);
+        $menu['Relatórios']->addChild('Bolsas', ['route' => 'relatorio_pagamento']);
+        $menu['Relatórios']->addChild('Bolsas não autorizadas', ['route' => 'relatorio_pagamento_nao_autorizado']);
+
         $menu->addChild('Gerenciar Sistema')->setAttribute('dropdown', true);
-        $menu['Gerenciar Sistema']->addChild('Abrir Cadastro Participante', array('route' => 'abertura_cadastro_participante'));
-        $menu['Gerenciar Sistema']->addChild('Manter Valor de Bolsa', array('route' => 'valor_bolsa_index'));
-        $menu['Gerenciar Sistema']->addChild('Planejar Abertura de Folha', array('route' => 'planejamento_abertura_folha'));
-        $menu['Gerenciar Sistema']->addChild('Manter Modelo de Certificado', array('route' => 'modelo_certificado', 'routeParameters' => ['filter' => 'clear']));
-        $menu['Gerenciar Sistema']->addChild('Manter Banco', ['route' => 'banco_index']);
-        $menu['Gerenciar Sistema']->addChild('Manter Administrador', ['route' => 'administrador']);
-        $menu['Gerenciar Sistema']->addChild('Manter Instituição', ['route' => 'instituicao']);
-        $menu['Gerenciar Sistema']->addChild('Manter Campus', ['route' => 'campus']);
-        $menu['Gerenciar Sistema']->addChild('Manter Cursos de Formação', ['route' => 'curso_formacao']);
-        $menu['Gerenciar Sistema']->addChild('Manter Texto Saudação', ['route' => 'saudacao']);
-
-        $menu->addChild('Folha de Pagamento/Arquivos Bancários')->setAttribute('dropdown', true);
-        $menu['Folha de Pagamento/Arquivos Bancários']->addChild('Retorno de Cadastro', array('route' => 'arquivo_retorno_cadastro'));
-        $menu['Folha de Pagamento/Arquivos Bancários']->addChild('Retorno de Pagamento', array('route' => 'arquivo_retorno_pagamento'));        
+        $menu['Gerenciar Sistema']->addChild('Abrir Cadastro Participante', ['route' => 'abertura_cadastro_participante']);
     }
-    
-    /**
-     * @param \Knp\Menu\ItemInterface $menu
-     */
-    private function menuCoordenadorProjeto(\Knp\Menu\ItemInterface $menu)
-    {
-        $projeto = $this->getProjetoAutenticado();
 
-        if ($projeto && $projeto->getPublicacao()->getPrograma()->isAreaAtuacao()) {
-            $menu->addChild('Grupos de atuação', array('route' => 'grupo_atuacao_index'));
+    private function menuCoordenadorProjeto(ItemInterface $menu)
+    {
+        if ($projeto = $this->getProjetoAutenticado()) {
+            if ($projeto->getPublicacao()->getPrograma()->isAreaAtuacao()) {
+                $menu->addChild('Grupos de atuação', ['route' => 'grupo_atuacao_index']);
+            }
         }
 
-        $menu->addChild('Participantes', array('route' => 'participante'));
-        $menu->addChild('Estabelecimentos', array('route' => 'estabelecimento_index'));
-        $menu->addChild('Autorização de pagamento', array('route' => 'autorizacao_pagamento'));
-        $menu->addChild('Certificado/Declaração', array('route' => 'certificado'));
-        
-        $menu->addChild('Relatorios')
-             ->setAttribute('dropdown', true);
-        $menu['Relatorios']->addChild('Gerencial de Participantes', array('route' => 'relatorio_gerencial_participante'));
-        
-        $menu->addChild('Gerenciar Sistema')
-             ->setAttribute('dropdown', true);
-        $menu['Gerenciar Sistema']->addChild('Planejar Abertura de Folha', array('route' => 'planejamento_abertura_folha'));
-        
-        $menu->addChild('Avaliação de Atividades')
-             ->setAttribute('dropdown', true);
-        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', array('route' => 'tramita_formulario_atividade_index_retorno'));
+        $menu->addChild('Participantes', ['route' => 'participante']);
+        $menu->addChild('Autorização de pagamento', ['route' => 'autorizacao_pagamento']);
     }
-    
-    /**
-     * 
-     * @param \Knp\Menu\ItemInterface $menu
-     */
-    private function menuCoordenadorGrupo(\Knp\Menu\ItemInterface $menu)
-    {
-        $menu->addChild('Avaliação de Atividades')
-             ->setAttribute('dropdown', true);
-        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', array('route' => 'tramita_formulario_atividade_index_retorno'));
-    }
-    
-    /**
-     * 
-     * @param \Knp\Menu\ItemInterface $menu
-     */
-    private function menuPreceptor(\Knp\Menu\ItemInterface $menu)
-    {
-        $menu->addChild('Avaliação de Atividades')
-             ->setAttribute('dropdown', true);
-        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', array('route' => 'tramita_formulario_atividade_index_retorno'));
-    }
-    
-    /**
-     * 
-     * @param \Knp\Menu\ItemInterface $menu
-     */
-    private function menuTutor(\Knp\Menu\ItemInterface $menu)
-    {
-        $menu->addChild('Avaliação de Atividades')
-             ->setAttribute('dropdown', true);
-        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', array('route' => 'tramita_formulario_atividade_index_retorno'));
-    }
-    
-    /**
-     * 
-     * @param \Knp\Menu\ItemInterface $menu
-     */
-    private function menuEstudante(\Knp\Menu\ItemInterface $menu)
-    {
-        $menu->addChild('Avaliação de Atividades')
-             ->setAttribute('dropdown', true);
-        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', array('route' => 'tramita_formulario_atividade_index_retorno'));
-    }
-    
-    /**
-     * 
-     * @return mixed
-     * @throws \LogicException
-     */
-    protected function getUser()
-    {
-        if (!$this->container->has('security.token_storage')) {
-            throw new \LogicException('The SecurityBundle is not registered in your application.');
-        }
 
-        if (null === $token = $this->container->get('security.token_storage')->getToken()) {
-            return;
-        }
+    private function menuCoordenadorGrupo(ItemInterface $menu)
+    {
+        $menu->addChild('Avaliação de Atividades')->setAttribute('dropdown', true);
+        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', ['route' => 'tramita_formulario_atividade_index_retorno']);
+    }
 
-        if (!is_object($user = $token->getUser())) {            
-            return;
+    private function menuPreceptor(ItemInterface $menu)
+    {
+        $menu->addChild('Avaliação de Atividades')->setAttribute('dropdown', true);
+        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', ['route' => 'tramita_formulario_atividade_index_retorno']);
+    }
+
+    private function menuTutor(ItemInterface $menu)
+    {
+        $menu->addChild('Avaliação de Atividades')->setAttribute('dropdown', true);
+        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', ['route' => 'tramita_formulario_atividade_index_retorno']);
+    }
+
+    private function menuEstudante(ItemInterface $menu)
+    {
+        $menu->addChild('Avaliação de Atividades')->setAttribute('dropdown', true);
+        $menu['Avaliação de Atividades']->addChild('Tramitar Formulário de Atividades', ['route' => 'tramita_formulario_atividade_index_retorno']);
+    }
+
+    protected function getUser(): ?UserInterface
+    {
+        $token = $this->tokenStorage->getToken();
+
+        if (!$token || !is_object($user = $token->getUser())) {
+            return null;
         }
 
         return $user;
     }
 
-    public function getProjetoAutenticado()
+    protected function getProjetoAutenticado(): ?Projeto
     {
-        if (null === $token = $this->container->get('security.token_storage')->getToken()) {
-            return;
+        $token = $this->tokenStorage->getToken();
+
+        if (!$token) {
+            return null;
         }
 
-        $projeto = null;
+        $attributes = $token->getAttributes();
 
-        if ($token->hasAttribute('coProjeto')) {
-            $id = $token->getAttribute('coProjeto');
-            if ($id) {
-                $projeto = $this->container->get('doctrine.orm.default_entity_manager')->find('App:Projeto', $id);
-            }
+        if (!isset($attributes['coProjeto']) || !$attributes['coProjeto']) {
+            return null;
         }
 
-        return $projeto;
+        return $this->entityManager->find(Projeto::class, $attributes['coProjeto']);
     }
 }
